6f151cea44ca81c1149fca78ee337f4f42ffe0c8
Def4ultUs3r <middle.it.developer@gmail.com>
2022-06-02 17:00:53 +0300

    Visual part

---
+++ MainForm.cs
@@ -0,0 +1,12 @@
+namespace DepositCalculator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            cbCurrency.SelectedIndex = 0;
+            cbMethod.SelectedIndex = 0;
+        }
+    }
+}
